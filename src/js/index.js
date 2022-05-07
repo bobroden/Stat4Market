586a4248acd0ef3dddba2867149ef5fa1c9c8a1a
bobroden <bobrow.bobro2015@yandex.ru>
2022-05-08 02:07:41 +0300

    add: initial markup

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import "../styles/index.scss";
+
+import "../assets/icons/logo.svg";
+import "../assets/icons/phone.svg";
+import "../assets/icons/login.svg";
+import "../assets/icons/price.svg";
+import "../assets/icons/advantages-image-1.svg";
+import "../assets/icons/advantages-image-2.svg";
+import "../assets/icons/advantages-image-3.svg";
+import "../assets/icons/advantages-image-4.svg";
